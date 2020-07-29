f766524d52fc821529e51aed98f1c3e4bc1a6bed
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-29 09:01:56 -0600

    Create test to display name

--- test/item_test.rb
+++ test/item_test.rb
@@ -9,4 +9,13 @@ class ItemTest < Minitest::Test
 
     assert_instance_of Item, item1
   end
+
+  def test_it_has_a_name
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    assert_equal 'Peach', item1.name
+    assert_equal 'Tomato', item2.name
+
+  end
 end
