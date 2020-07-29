98f531d7141844b45387dc42787582330c7123c4
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-29 09:19:15 -0600

    Pass test of having numerous items in inventory

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -41,6 +41,23 @@ class VendorTest < Minitest::Test
     expected = {item1=>30}
     assert_equal expected, vendor.inventory
     assert_equal 30, vendor.check_stock(item1)
-
   end
+
+  def test_it_can_have_numerous_items_in_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    vendor.stock(item1, 30)
+    vendor.stock(item2, 12)
+
+    expected = {item1=>30, item2=>12}
+
+    assert_equal expected, vendor.inventory
+    assert_equal 12, vendor.check_stock(item2)
+  end
+
+
+
 end
