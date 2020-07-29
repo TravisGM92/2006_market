423fd618bbb937cdc12d47e29e0811d6da517438
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-29 09:25:47 -0600

    Create test to display vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -19,4 +19,10 @@ class MarketTest < Minitest::Test
 
     assert_equal "South Pearl Street Farmers Market", market.name
   end
+
+  def test_it_has_no_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_equal [], market.vendors
+  end
 end
